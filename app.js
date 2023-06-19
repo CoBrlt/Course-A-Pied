f6952aa32b177f44d0fa55d5908ac3a8779fcd52
Podobox <jeremie.sneed@yahoo.fr>
2023-06-19 10:10:42 +0200

    javascript

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Bonjour monde")
+    // function display_by_id (id)
+    // {
+        
+    // }
+
+function hidden_by_id (id)
+{
+    const x = document.getElementById("chrono_min_input");
+    console.log(x);
+}
+hidden_by_id("affichage_vma");
