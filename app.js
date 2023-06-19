e78cb27607eefa2cc2b364fc35a2ea28f77097f5
Podobox <jeremie.sneed@yahoo.fr>
2023-06-19 15:11:21 +0200

    Chronometre + reset picture

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
 var divs = document.getElementsByTagName("div");
+const sec = document.getElementById("time");
+let timer = 0;
+let isRunning = false;
+let interval;
+
 function hide_all () {
     for (let i=0; i < divs.length; i++)
     {
@@ -24,6 +29,44 @@ function hidden_by_id (id)
 {
     document.getElementById(id).style.display = "none";
 }
+
+function pad(number){
+    return (number <10) ? "0"+number : number;
+}
+
+function incrementTimer() {
+    timer++;
+
+    const numberMin = Math.floor(timer / 6000);
+    const numberSec = parseInt((timer % 6000)/100);
+    const numbercent = timer % 100;
+    sec.innerHTML = pad(numberMin)+ ":" + pad(numberSec)+","+ pad(numbercent);
+    
+  }
+
+function resetChrono()
+{
+    if(isRunning) {
+        isRunning = false;
+        clearInterval(interval);
+    }
+    timer = 0;
+    sec.innerHTML = "00:00";
+
+}
+
+function startChrono()
+{
+    if(isRunning) {
+        isRunning = false;
+        clearInterval(interval);
+    }
+    else{
+        isRunning = true;
+        interval = setInterval(incrementTimer, 10)
+    }
+}
+
 hide_all();
 
 document.getElementById("chrono_button").addEventListener("click", function(){
@@ -59,10 +102,15 @@ document.getElementById("back_button").addEventListener("click", function(){
     display_by_id("affichage_vma");
     display_by_id("menu");
 });
+
+//chrono
+document.getElementById("start_pause_button").addEventListener("click", startChrono);
+
+document.getElementById("reset_tour_button").addEventListener("click", resetChrono);
+
 //display the menu
 display_by_id("affichage_vma");
 display_by_id("menu");
-
 /*
 Liste des choses à faire :
 
