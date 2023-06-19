2874eb6e270bcfb0f90a153b4afa56ff0c286614
Podobox <jeremie.sneed@yahoo.fr>
2023-06-19 11:30:12 +0200

    Listener javascript

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-console.log("Bonjour monde")
-    // function display_by_id (id)
-    // {
-        
-    // }
+
+var divs = document.getElementsByTagName("div");
+function hide_all () {
+    for (let i=0; i < divs.length; i++)
+    {
+        divs[i].style.display = "none";
+    }
+}
+
+function display_by_id (id)
+{
+    const monElement = document.getElementById(id);
+    monElement.style.display = "block";
+    if (monElement.children.length != 1){
+        for (let i = 0; i < monElement.children.length; i++) {
+            display_by_id(monElement.children[i].id);
+        }
+    }
+}
 
 function hidden_by_id (id)
 {
-    const x = document.getElementById("chrono_min_input");
-    console.log(x);
+    document.getElementById(id).style.display = "none";
 }
-hidden_by_id("affichage_vma");
+hide_all();
+document.getElementById("chrono_button").addEventListener("click", function(){
+    hide_all();
+    display_by_id("chrono");
+});
+document.getElementById("duree_button").addEventListener("click", function(){
+    hide_all();
+    display_by_id("calculator");
+    hidden_by_id("duree_label_input");
+});
+document.getElementById("vitesse_button").addEventListener("click", function(){
+    hide_all();
+    display_by_id("calculator");
+    hidden_by_id("distance_label_input");
+});
+document.getElementById("distance_button").addEventListener("click", function(){
+    hide_all();
+    display_by_id("calculator");
+    hidden_by_id("distance_label_input");
+});
+//display the menu
+display_by_id("affichage_vma");
+display_by_id("menu");
+
