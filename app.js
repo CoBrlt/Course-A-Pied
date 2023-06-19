455981b0f383bea407bf2b1a922b0e2e1236d097
Podobox <jeremie.sneed@yahoo.fr>
2023-06-19 11:52:47 +0200

    push

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ function hide_all () {
 
 function display_by_id (id)
 {
+    console.log(id);
     const monElement = document.getElementById(id);
     monElement.style.display = "block";
+    //console.log(monElement);
     if (monElement.children.length != 1){
         for (let i = 0; i < monElement.children.length; i++) {
             display_by_id(monElement.children[i].id);
@@ -23,24 +25,39 @@ function hidden_by_id (id)
     document.getElementById(id).style.display = "none";
 }
 hide_all();
+
 document.getElementById("chrono_button").addEventListener("click", function(){
     hide_all();
     display_by_id("chrono");
+    display_by_id("back_button");
 });
+
 document.getElementById("duree_button").addEventListener("click", function(){
     hide_all();
     display_by_id("calculator");
+    display_by_id("back_button");
     hidden_by_id("duree_label_input");
 });
+
 document.getElementById("vitesse_button").addEventListener("click", function(){
     hide_all();
     display_by_id("calculator");
+    display_by_id("back_button");
     hidden_by_id("distance_label_input");
 });
+
 document.getElementById("distance_button").addEventListener("click", function(){
     hide_all();
     display_by_id("calculator");
+    display_by_id("back_button");
     hidden_by_id("distance_label_input");
+});
+
+document.getElementById("back_button").addEventListener("click", function(){
+    hide_all();
+    //display the menu
+    display_by_id("affichage_vma");
+    display_by_id("menu");
 });
 //display the menu
 display_by_id("affichage_vma");
