afb695df2914a6d66826a265b44074587ca738a0
Essassi Ali <ali.essassi@insa-cvl.com>
2023-06-19 15:55:40 +0200

    calculs time, speed and distance

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 
 var divs = document.getElementsByTagName("div");
+var vitesse_text;
+var distance_text;
+var duree_text;
+var duree_min_text;
+var duree_sec_text;
+var allure_text;
 function hide_all () {
     for (let i=0; i < divs.length; i++)
     {
@@ -37,13 +43,15 @@ document.getElementById("duree_button").addEventListener("click", function(){
     display_by_id("calculator");
     display_by_id("back_button");
     hidden_by_id("duree_label_input");
+    add_listener_duree();
 });
 
 document.getElementById("vitesse_button").addEventListener("click", function(){
     hide_all();
     display_by_id("calculator");
     display_by_id("back_button");
-    hidden_by_id("distance_label_input");
+    hidden_by_id("allure_label_input");
+    add_listener_vitesse();
 });
 
 document.getElementById("distance_button").addEventListener("click", function(){
@@ -51,6 +59,7 @@ document.getElementById("distance_button").addEventListener("click", function(){
     display_by_id("calculator");
     display_by_id("back_button");
     hidden_by_id("distance_label_input");
+    add_listener_distance();
 });
 
 document.getElementById("back_button").addEventListener("click", function(){
@@ -63,6 +72,108 @@ document.getElementById("back_button").addEventListener("click", function(){
 display_by_id("affichage_vma");
 display_by_id("menu");
 
+function add_listener_duree(){
+    document.getElementById("distance_input").addEventListener("change", function(){
+        distance_text = document.getElementById("distance_input").value;
+        vitesse_text = document.getElementById("allure_input").value;
+        console.log(distance_text);
+        console.log(vitesse_text);
+        if(distance_text != "" && vitesse_text != ""){
+            duree_text = Math.round(distance_text*100/vitesse_text)/100;
+            document.getElementById("result").textContent = duree_text;
+        }
+        console.log(duree_text);
+    });
+}
+function add_listener_vitesse(){
+    document.getElementById("distance_input").addEventListener("change", function(){
+        distance_text = document.getElementById("distance_input").value;
+        duree_min_text = document.getElementById("duree_min_input").value;
+        duree_sec_text= document.getElementById("duree_sec_input").value;
+        duree_text = parseInt(duree_min_text)*60 + parseInt(duree_sec_text);
+        console.log(distance_text); 
+        console.log(duree_text);
+        if(distance_text != "" && duree_min_text != "" && duree_sec_text != ""){
+            vitesse_text = Math.round(distance_text*3.6*100/duree_text)/100;
+            document.getElementById("result").textContent = vitesse_text;
+        }
+        console.log(vitesse_text);
+    });
+
+    document.getElementById("duree_min_input").addEventListener("change", function(){
+        distance_text = document.getElementById("distance_input").value;
+        duree_min_text = document.getElementById("duree_min_input").value;
+        duree_sec_text= document.getElementById("duree_sec_input").value;
+        duree_text = parseInt(duree_min_text)*60 + parseInt(duree_sec_text);
+        console.log(distance_text); 
+        console.log(duree_text);
+        if(distance_text != "" && duree_min_text != "" && duree_sec_text != ""){
+            vitesse_text = Math.round(distance_text*3.6*100/duree_text)/100;
+            document.getElementById("result").textContent = vitesse_text;
+        }
+        console.log(vitesse_text);
+    });
+
+    document.getElementById("duree_sec_input").addEventListener("change", function(){
+        distance_text = document.getElementById("distance_input").value;
+        duree_min_text = document.getElementById("duree_min_input").value;
+        duree_sec_text= document.getElementById("duree_sec_input").value;
+        duree_text = parseInt(duree_min_text)*60 + parseInt(duree_sec_text);
+        console.log(distance_text); 
+        console.log(duree_text);
+        if(distance_text != "" && duree_min_text != "" && duree_sec_text != ""){
+            vitesse_text = Math.round(distance_text*3.6*100/duree_text)/100;
+            document.getElementById("result").textContent = vitesse_text;
+        }
+        console.log(vitesse_text);
+    });
+}
+function add_listener_distance(){
+    document.getElementById("allure_input").addEventListener("change", function(){
+        vitesse_text = document.getElementById("allure_input").value;
+        duree_min_text = document.getElementById("duree_min_input").value;
+        duree_sec_text= document.getElementById("duree_sec_input").value;
+        duree_text = parseInt(duree_min_text)*60 + parseInt(duree_sec_text);
+        console.log(vitesse_text); 
+        console.log(duree_text);
+        if(vitesse_text != "" && duree_min_text != "" && duree_sec_text != ""){
+            distance_text = parseInt(vitesse_text)*parseInt(duree_text);
+            document.getElementById("result").textContent = distance_text;
+            
+        }
+        console.log(vitesse_text);
+    });
+
+    document.getElementById("duree_min_input").addEventListener("change", function(){
+        vitesse_text = document.getElementById("allure_input").value;
+        duree_min_text = document.getElementById("duree_min_input").value;
+        duree_sec_text= document.getElementById("duree_sec_input").value;
+        duree_text = parseInt(duree_min_text)*60 + parseInt(duree_sec_text);
+        console.log(vitesse_text); 
+        console.log(duree_text);
+        if(vitesse_text != "" && duree_min_text != "" && duree_sec_text != ""){
+            distance_text = parseInt(vitesse_text)*parseInt(duree_text)/3.6;
+            document.getElementById("result").textContent = distance_text;
+            
+        }
+        console.log(vitesse_text);
+    });
+
+    document.getElementById("duree_sec_input").addEventListener("change", function(){
+        vitesse_text = document.getElementById("allure_input").value;
+        duree_min_text = document.getElementById("duree_min_input").value;
+        duree_sec_text= document.getElementById("duree_sec_input").value;
+        duree_text = parseInt(duree_min_text)*60 + parseInt(duree_sec_text);
+        console.log(vitesse_text); 
+        console.log(duree_text);
+        if(vitesse_text != "" && duree_min_text != "" && duree_sec_text != ""){
+            distance_text = parseInt(vitesse_text)*parseInt(duree_text)/3.6;
+            document.getElementById("result").textContent = distance_text;
+            
+        }
+        console.log(vitesse_text);
+    });
+}
 /*
 Liste des choses à faire :
 
